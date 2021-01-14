2d33784617b65c404ac62434c12e18e127d4eed4
Tech305 <zbigmoney305@gmail.com>
2021-01-14 14:30:03 -0600

    Update

---
+++ Nami/Events.cs
@@ -0,0 +1,62 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nami
+{
+    internal class Events
+    {
+        internal static Task OnMemberAdded(DiscordClient sender, GuildMemberAddEventArgs e)
+        {
+            Task.Run(async () =>
+            {
+                var channel = e.Guild.Channels.ToList().Find(x => x.Value.Name.ToLower().Contains("announce")).Value;
+                if (channel == null) channel = e.Guild.SystemChannel;
+                var embed = new DiscordEmbedBuilder();
+                embed.Timestamp = new DateTimeOffset(DateTime.Now);
+                embed.Title = "Member Join";
+                embed.Color = DiscordColor.Green;
+                embed.Description = $"{e.Member.DisplayName} has joined, lest welcome them!";
+                embed.Footer = new DiscordEmbedBuilder.EmbedFooter()
+                {
+                    Text = $"Thank you for your time. {e.Guild.Name} Team",
+                };
+                await channel.SendMessageAsync(embed: embed.Build());
+            });
+            return Task.CompletedTask;
+        }
+        internal static Task OnMemberRemoved(DiscordClient sender, GuildMemberRemoveEventArgs e)
+        {
+            Task.Run(async () =>
+            {
+                var channel = e.Guild.Channels.ToList().Find(x => x.Value.Name.ToLower().Contains("announce")).Value;
+                if (channel == null) channel = e.Guild.SystemChannel;
+                var embed = new DiscordEmbedBuilder();
+                embed.Timestamp = new DateTimeOffset(DateTime.Now);
+                embed.Title = "Member Left";
+                embed.Color = DiscordColor.Red;
+                embed.Description = $"{e.Member.DisplayName} has left, so sad to see you go";
+                embed.Footer = new DiscordEmbedBuilder.EmbedFooter()
+                {
+                    Text = $"Thank you for your time. {e.Guild.Name} Team",
+                };
+                await channel.SendMessageAsync(embed: embed.Build());
+            });
+            return Task.CompletedTask;
+        }
+
+        internal static Task OnGuildsAvailable(DiscordClient sender, GuildCreateEventArgs e)
+        {
+            Task.Run(async () => 
+            {
+                DiscordServer.servers.Add(new DiscordServer(e.Guild));
+                Console.WriteLine($"{e.Guild.Name} is now registered ● {DateTime.Now}");
+                await Task.CompletedTask;
+            });
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Nami/ResourceFile.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Nami
+{
+    public class ResourceFile<T>
+    {
+        public Dictionary<string, object> data = new Dictionary<string, object>()
+        {
+            { "ftr",  true},
+            { "prefixes", new[] { "?", "-",  "*" } },
+        };
+        public T this[string key, T _default = default]
+        {
+            get
+            {
+                key = key.ToLower();
+                key = key.Replace(' ', '-');
+
+                Load();
+                if (data.ContainsKey(key)) return (T)data[key];
+                return _default != null ? _default : default;
+            }
+            set
+            {
+                Load();
+                key = key.ToLower();
+                key = key.Replace(' ', '-');
+
+                if (data.ContainsKey(key))
+                    data[key] = value;
+                if (!data.ContainsKey(key))
+                    data.Add(key, value);
+                Save();
+            }
+        }
+
+        private void Load()
+        {
+            var resoruceFile = "Resources/resources.r";
+            Directory.CreateDirectory("Resources");
+            var bf = new BinaryFormatter();
+            if (!File.Exists(resoruceFile)) return;
+            using (var fs = new FileStream(resoruceFile, FileMode.Open, FileAccess.Read))
+            {
+                data = (Dictionary<string, object>)bf.Deserialize(fs);
+            }
+        }
+        private void Save()
+        {
+            var resoruceFile = "Resources/resources.r";
+            Directory.CreateDirectory("Resources");
+            var bf = new BinaryFormatter();
+            using (var fs = new FileStream(resoruceFile, FileMode.Create, FileAccess.Write))
+            {
+                bf.Serialize(fs, data);
+            }
+        }
+    }
+}
