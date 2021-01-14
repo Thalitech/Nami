7da6a6fc4c437128eda27ee44144f030c6c69ff6
Tech305 <zbigmoney305@gmail.com>
2021-01-14 11:14:05 -0600

    remaking project

--- Nami/Program.cs
+++ Nami/Program.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using DSharpPlus;
+using System;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Nami
@@ -10,6 +9,29 @@ namespace Nami
     {
         static void Main(string[] args)
         {
+            Console.Title = Assembly.GetExecutingAssembly().GetName().Name;
+            if(new ResourceFile<bool>()["ftr"])
+            {
+                Console.WriteLine("You have not setup your discord token.");
+                Console.Write("Discord Bot Token: ");
+                var token = Console.ReadLine();
+                if(!string.IsNullOrEmpty(token))
+                {
+                    new ResourceFile<string>()["token"] = token;
+                }
+                new ResourceFile<bool>()["ftr"] = false;
+            }    
+
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
+
+        private async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = new ResourceFile<string>()["token", "your-discord-token"],
+                TokenType = TokenType.Bot
+            });
         }
     }
 }
