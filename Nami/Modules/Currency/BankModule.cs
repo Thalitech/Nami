56b354eaea2abec02f3ff578ff36c978786b5c7f
Tech305 <zbigmoney305@gmail.com>
2021-01-29 16:27:39 -0600

    Auto Push [01/29/2021][16:27:38.80] (Debug)

--- Nami/Modules/Currency/BankModule.cs
+++ Nami/Modules/Currency/BankModule.cs
@@ -245,21 +245,21 @@ namespace Nami.Modules.Currency
             var member = _member ?? ctx.Member;
             var config = await Cooldown.LoadConfigAsync();
 
-
             var working_member = config.Find(ctx.Guild, member);
 
-
-
-            if (working_member == null) {
-                await this.Service.IncreaseBankAccountAsync(ctx.Guild.Id, (ulong)member.Id, 1000);
+            if (working_member == null) 
+            {
                 config.Add(ctx.Guild, member, DateTime.Now.AddDays(1));
+                await this.Service.IncreaseBankAccountAsync(ctx.Guild.Id, (ulong)member.Id, 1000);
                 await ctx.ImpInfoAsync(this.ModuleColor, Emojis.MoneyBag, "fmt-bank-allow", member.Mention, 1000, config.Find(ctx.Guild, member).cooldaown);
-            } else {
+            } 
+            else
+            {
                 if (working_member.cooldaown.Date == DateTime.Now.Date && working_member.cooldaown.Date != working_member.created.Date) 
                 {
-                    await this.Service.IncreaseBankAccountAsync(ctx.Guild.Id, (ulong)_member.Id, 1000);
                     config.Find(ctx.Guild, member).cooldaown.AddDays(1);
                     config.Save();
+                    await this.Service.IncreaseBankAccountAsync(ctx.Guild.Id, (ulong)_member.Id, 1000);
                     await ctx.ImpInfoAsync(this.ModuleColor, Emojis.MoneyBag, "fmt-bank-allow", ctx.User.Mention, 1000, config.Find(ctx.Guild, ctx.Member).cooldaown);
                 }
                 else
