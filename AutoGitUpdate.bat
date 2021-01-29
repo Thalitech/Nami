d6e5badab200781baf7bd157f9640bc408dc6291
Tech305 <zbigmoney305@gmail.com>
2021-01-28 22:53:49 -0600

    Auto Push [01/28/2021][22:53:49.13]

--- AutoGitUpdate.bat
+++ AutoGitUpdate.bat
@@ -4,7 +4,7 @@ for /F "tokens=2" %%i in ('date /t') do set mydate=%%i
 set mytime=%time%
 
 git add *
-git commit --all -m "Auto Push %mydate% %mytime%"
+git commit --all -m "Auto Push [%mydate%][%mytime%]"
 git push
 echo closeing in 10s
 sleep 10000
