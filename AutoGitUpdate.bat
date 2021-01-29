436e874dbe7125737d6f4aa4046add9e60f75fe4
Tech305 <zbigmoney305@gmail.com>
2021-01-28 23:06:51 -0600

    Auto Push [01/28/2021][23:06:50.39]

--- AutoGitUpdate.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Auto Git pusher
-for /F "tokens=2" %%i in ('date /t') do set mydate=%%i
-set mytime=%time%
-
-git add *
-git commit --all -m "Auto Push [%mydate%][%mytime%]"
-git push
